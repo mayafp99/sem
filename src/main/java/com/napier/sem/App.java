5fcfa1bd0e42e2117debec9ff86081715b6a5215
mayafp99 <mayafp99@gmail.com>
2023-02-03 10:16:16 +0000

    seventh commit. database testing.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,8 +1,30 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 public class App {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
+
+        // Connect to MongoDB on a local system. We're using port 27000.
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Get a database. Will create when we use it.
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        // Get a collection from the database.
+        MongoCollection<Document> collection = database.getCollection("test");
+        // Create a document to store.
+        Document doc = new Document("name", "Maya Peretz")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2023")
+                .append("result", new Document("CW", 65).append("EX", 85));
+        // Add document to the collection.
+        collection.insertOne(doc);
+
+        //Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
